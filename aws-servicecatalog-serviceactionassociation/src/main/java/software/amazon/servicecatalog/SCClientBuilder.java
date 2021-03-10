3622f89663b0f73fd1f13a7e0a6aaba3050e05e7
divishaj <78000823+divishaj@users.noreply.github.com>
2021-03-10 13:07:02 -0800

    Adding getRole permission in schema and user agent suffix in SCClient builder (#5)

--- aws-servicecatalog-serviceactionassociation/src/main/java/software/amazon/servicecatalog/SCClientBuilder.java
+++ aws-servicecatalog-serviceactionassociation/src/main/java/software/amazon/servicecatalog/SCClientBuilder.java
@@ -1,14 +1,23 @@
 package software.amazon.servicecatalog;
 
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.services.servicecatalog.ServiceCatalogClient;
 import software.amazon.cloudformation.LambdaWrapper;
 
+import static software.amazon.awssdk.core.client.config.SdkAdvancedClientOption.USER_AGENT_SUFFIX;
+
 public class SCClientBuilder {
+    private static final String CFN_USER_AGENT_SUFFIX = "CFN_Resource_Generated";
 
     public static ServiceCatalogClient getClient() {
+        final ClientOverrideConfiguration overrideConfiguration = ClientOverrideConfiguration
+                .builder()
+                .putAdvancedOption(USER_AGENT_SUFFIX, CFN_USER_AGENT_SUFFIX)
+                .build();
         return ServiceCatalogClient
                 .builder()
                 .httpClient(LambdaWrapper.HTTP_CLIENT)
+                .overrideConfiguration(overrideConfiguration)
                 .build();
     }
 }
